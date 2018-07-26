67193c69e31032c3804cde17375f60be15f2c894
Neronse <n.a.vasilchenko@yandex.ru>
2018-07-26 16:55:53 +0300

    minifyEnabled true Valute -> Currency Локализация operationType переделан из boolean в enum

--- app/src/main/java/com/yaschool/neronse/yft/data/Operation.kt
+++ app/src/main/java/com/yaschool/neronse/yft/data/Operation.kt
@@ -3,12 +3,17 @@ package com.yaschool.neronse.yft.data
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-data class Operation(val operationType: Boolean, val count: BigDecimal, val valute: Valute ) {
+data class Operation(val operationType: OperationType, val count: BigDecimal, val currency: Currency ) {
     init {
         count.setScale(2, RoundingMode.HALF_EVEN)
     }
 }
-enum class Valute{
+enum class Currency{
     RUB,
     DOLLAR
+}
+
+enum class OperationType{
+    INCOME,
+    EXPENSE
 }
--- app/src/test/java/com/yaschool/neronse/yft/FinanceUnitTest.kt
+++ app/src/test/java/com/yaschool/neronse/yft/FinanceUnitTest.kt
@@ -1,7 +1,8 @@
 package com.yaschool.neronse.yft
 
+import com.yaschool.neronse.yft.data.Currency
 import com.yaschool.neronse.yft.data.Operation
-import com.yaschool.neronse.yft.data.Valute
+import com.yaschool.neronse.yft.data.OperationType
 import com.yaschool.neronse.yft.mathematics.calculateBalance
 import org.junit.Test
 
@@ -10,14 +11,14 @@ import java.math.BigDecimal
 
 class FinanceUnitTest {
     val list: List<Operation> = listOf(
-            Operation(false, BigDecimal("1.51"), Valute.RUB),
-            Operation(true, BigDecimal("1.51"), Valute.RUB),
-            Operation(false, BigDecimal("1.51"), Valute.RUB),
-            Operation(true, BigDecimal("1.51"), Valute.RUB))
+            Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.RUB),
+            Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB),
+            Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.RUB),
+            Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB))
 
     val list2: List<Operation> = listOf(
-            Operation(false, BigDecimal("1.51"), Valute.RUB),
-            Operation(true, BigDecimal("1.51"), Valute.DOLLAR))
+            Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB),
+            Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.DOLLAR))
 
 
     @Test
@@ -28,7 +29,7 @@ class FinanceUnitTest {
         try{
             calculateBalance(list2)
         }catch (e: IllegalArgumentException ){
-            assertEquals(e.message, "Only one valute in list")
+            assertEquals(e.message, "Only one currency in list")
         }
     }
 }
