013b1afa9a562f63ca36c73728275f086b925d8b
Neronse <n.a.vasilchenko@yandex.ru>
2018-07-30 09:50:01 +0300

    added test calculateExpense

--- app/src/test/java/com/yaschool/neronse/yft/FinanceUnitTest.kt
+++ app/src/test/java/com/yaschool/neronse/yft/FinanceUnitTest.kt
@@ -5,22 +5,23 @@ import com.yaschool.neronse.yft.data.Currency
 import com.yaschool.neronse.yft.data.Operation
 import com.yaschool.neronse.yft.data.OperationType
 import com.yaschool.neronse.yft.mathematics.calculateBalance
+import com.yaschool.neronse.yft.mathematics.calculateExpense
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.math.BigDecimal
 
 class FinanceUnitTest {
-    val list: List<Operation> = listOf(
+    private val list: List<Operation> = listOf(
             Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.RUB, Category.OTHER, "comment"),
             Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB, Category.OTHER, "comment"),
             Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.RUB, Category.OTHER, "comment"),
             Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB, Category.OTHER, "comment"))
 
-    val list2: List<Operation> = listOf(
+    private val list2: List<Operation> = listOf(
             Operation(OperationType.EXPENSE, BigDecimal("1.51"), Currency.RUB, Category.OTHER, "comment"),
             Operation(OperationType.INCOME, BigDecimal("1.51"), Currency.DOLLAR, Category.OTHER, "comment"))
 
-    val list3: List<Operation> = listOf(
+    private val list3: List<Operation> = listOf(
             Operation(OperationType.INCOME, BigDecimal("1.512681"), Currency.RUB, Category.OTHER, "comment"),
             Operation(OperationType.INCOME, BigDecimal("1.512681"), Currency.RUB, Category.OTHER, "comment"))
 
@@ -43,5 +44,11 @@ class FinanceUnitTest {
     fun testCalculateBalance3() {
         val result = calculateBalance(list3)
         assertEquals(result,"3.02")
+    }
+
+    @Test
+    fun testCalculateExpense(){
+        val result = calculateExpense(list.filter { it.operationType == OperationType.EXPENSE })
+        assertEquals(result, 3.02f)
     }
 }
